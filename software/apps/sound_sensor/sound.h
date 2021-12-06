934c4bc4c53810dc0ff0c2120d089b5434195777
MDecarabas <codrea.eric@gmail.com>
2021-12-06 14:23:29 -0600

    Sound works in loop

--- software/apps/sound_sensor/sound.h
+++ software/apps/sound_sensor/sound.h
@@ -1,12 +1,7 @@
-// sound sensor header file
-
-#include "nrf_twi_mngr.h"
-
-// Light sensor i2c address
-static const uint8_t LIGHT_ADDRESS = 0x5C;
 
 // Read the sound sensed
 int read_sound(void);
 
 // Initialize sound sensor
 void sound_init(void);
+
